1020ab6e217df46160b534fe86c793441eb3c324
Jon Phillips <jon@jonphillips.com>
2022-12-01 09:45:00 -0500

    include additional content

--- MovieTracker/Models/Movie.swift
+++ MovieTracker/Models/Movie.swift
@@ -3,7 +3,8 @@ import Foundation
 struct Movie: Identifiable, Hashable {
   var id: Int
   var title: String
-  var details: String?
+  var synopsis: String?
+  var directedBy: String?
   var genre: Genre
   var performers: [Performer] = []
   var posterUrl: URL?
@@ -23,23 +24,28 @@ extension Movie {
 
 extension Movie {
   static let previewData = [
-    Movie(id: 1, title: "The Power of the Dog", genre: .drama, performers: [.cumberbatch], posterUrl: PU.powerUrl),
-    Movie(id: 2, title: "Dune", genre: .sff, performers: [.chalamet, .zendaya], posterUrl: PU.duneUrl),
-    Movie(id: 3, title: "Little Women", genre: .drama, performers: [.ronan, .chalamet], posterUrl: PU.littleUrl),
-    Movie(id: 4, title: "The Harder They Fall", genre: .western, performers: [.elba, .king], posterUrl: PU.harderUrl),
-    Movie(id: 5, title: "Spider-Man: No Way Home", genre: .sff, performers: [.zendaya, .holland], posterUrl: PU.spiderUrl),
-    Movie(id: 6, title: "The Farewell", genre: .drama, performers: [.awkwafina], posterUrl: PU.farewellUrl)
+    Movie(id: 1, title: "Everything Everywhere All at Once", synopsis: "When an interdimensional rupture unravels reality, an unlikely hero must channel her newfound powers to fight bizarre and bewildering dangers from the multiverse as the fate of the world hangs in the balance.", directedBy: "Daniel Kwan and Daniel Scheinert", genre: .drama, performers: [.cumberbatch], posterUrl: PU.everythingUrl),
+    Movie(id: 2, title: "The Power of the Dog", directedBy: "Jane Campion", genre: .drama, performers: [.cumberbatch], posterUrl: PU.powerUrl),
+    Movie(id: 3, title: "Dune", directedBy: "Denis Villeneuve", genre: .sff, performers: [.chalamet, .zendaya], posterUrl: PU.duneUrl),
+    Movie(id: 4, title: "Little Women", directedBy: "Greta Gerwig", genre: .drama, performers: [.ronan, .chalamet], posterUrl: PU.littleUrl),
+    Movie(id: 5, title: "NOPE", directedBy: "Jordan Peele", genre: .sff, performers: [.kaluuya, .palmer, .yeun], posterUrl: PU.nopeUrl),
+    Movie(id: 6, title: "The Harder They Fall", genre: .western, performers: [.elba, .king], posterUrl: PU.harderUrl),
+    Movie(id: 7, title: "Spider-Man: No Way Home", genre: .sff, performers: [.zendaya, .holland], posterUrl: PU.spiderUrl),
+    Movie(id: 8, title: "The Farewell", synopsis: "A Chinese family discovers their grandmother has only a short while left to live and decide to keep her in the dark, scheduling a wedding to gather before she dies.", genre: .drama, performers: [.awkwafina], posterUrl: PU.farewellUrl),
+    Movie(id: 9, title: "Minari", synopsis: "In 1983, the Korean immigrant Yi family moves from California to their new plot of land in rural Arkansas, where father Jacob hopes to grow Korean produce to sell to vendors in Dallas. ", genre: .drama, performers: [.yeun], posterUrl: PU.minariUrl)
   ]
 }
 
 // This is silly; no one would ever do it like this. PU = "Poster Url"
 // I'm shoving these "constants" here to make the above dummyData creation easier to read
 struct PU {
+  static let everythingUrl = URL(string: "https://education-jrp.s3.amazonaws.com/MovieImages/EverythingEverywhereAllAtOnce.jpg")
   static let powerUrl = URL(string: "https://education-jrp.s3.amazonaws.com/MovieImages/ThePowerOfTheDog.jpeg")
+  static let nopeUrl = URL(string: "https://education-jrp.s3.amazonaws.com/MovieImages/Nope.jpg")
   static let duneUrl = URL(string: "https://education-jrp.s3.amazonaws.com/MovieImages/Dune.jpeg")
   static let littleUrl = URL(string: "https://education-jrp.s3.amazonaws.com/MovieImages/LittleWomen.jpeg")
   static let harderUrl = URL(string: "https://education-jrp.s3.amazonaws.com/MovieImages/TheHarderTheyFall.jpeg")
+  static let minariUrl = URL(string: "https://education-jrp.s3.amazonaws.com/MovieImages/Minari.png")
   static let spiderUrl = URL(string: "https://education-jrp.s3.amazonaws.com/MovieImages/SpiderMan.jpeg")
   static let farewellUrl = URL(string: "https://education-jrp.s3.amazonaws.com/MovieImages/TheFarewell.jpeg")
 }
-
--- MovieTracker/Models/Performer.swift
+++ MovieTracker/Models/Performer.swift
@@ -12,14 +12,18 @@ extension Performer {
     formatter.dateFormat = "MM/dd/yyyy"
     return formatter.date(from: date)!
   }
-  
+
   static let awkwafina = Performer(name: "Awkwafina", birthDate: birthDate("06/02/1988"))
   static let bacon = Performer(name: "Kevin Bacon", birthDate: nil)
   static let chalamet = Performer(name: "Timothée Chalamet", birthDate: birthDate("12/27/1995"))
   static let cumberbatch = Performer(name: "Benedict Cumberbatch", birthDate: birthDate("07/19/1976"))
   static let elba = Performer(name: "Idris Elba", birthDate: birthDate("09/06/1972"))
   static let holland = Performer(name: "Tom Holland", birthDate: birthDate("06/01/1996"))
+  static let kaluuya = Performer(name: "Daniel Kaluuya", birthDate: birthDate("02/24/1989"))
   static let king = Performer(name: "Regina King", birthDate: birthDate("01/15/1971"))
+  static let palmer = Performer(name: "Keke Palmer", birthDate: birthDate("08/26/1993"))
   static let ronan = Performer(name: "Saoirse Ronan", birthDate: birthDate("04/12/1994"))
+  static let yeoh = Performer(name: "Michelle Yeoh", birthDate: birthDate("08/06/1962"))
+  static let yeun = Performer(name: "Steven Yeun", birthDate: birthDate("12/21/1983"))
   static let zendaya = Performer(name: "Zendaya", birthDate: birthDate("09/01/1996"))
 }
