cd07773db29e12d3d2b7f80efb6b7257090f39e8
Jon Phillips <jon@jonphillips.com>
2023-11-28 20:37:01 -0500

    change navigation

--- MovieTracker/Features/MovieList.swift
+++ MovieTracker/Features/MovieList.swift
@@ -6,27 +6,30 @@ struct MovieList: View {
   var body: some View {
     NavigationStack {
       List(movies) { movie in
-        NavigationLink(destination: MovieDetail(movie: movie)) {
+        NavigationLink(value: movie) {
           HStack(alignment: .top) {
-            AsyncImage(url: movie.posterUrl, content: { image in
+            AsyncImage(url: movie.posterUrl) { image in
               image
                 .resizable()
                 .aspectRatio(contentMode: .fit)
-            }, placeholder: {
+            } placeholder: {
               if movie.posterUrl != nil {
                 ProgressView()
               } else {
                 Image(systemName: "film.fill")
               }
-            })
+            }
             .frame(maxWidth: 100, maxHeight: 100)
             Text(movie.title)
               .font(.title2)
           }
         }
       }
-      .padding()
+      .navigationDestination(for: Movie.self) { movie in
+        MovieDetail(movie: movie)
+      }
       .navigationTitle("Movies")
+      .padding()
       .listStyle(.plain)
     }
 
