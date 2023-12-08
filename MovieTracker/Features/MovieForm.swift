1e383e7fba3ab27c90b9a45a2ab8b8b021e54e86
Jon Phillips <jon@jonphillips.com>
2023-12-08 16:27:11 -0500

    adding performers

--- MovieTracker/Features/MovieForm.swift
+++ MovieTracker/Features/MovieForm.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 struct MovieForm: View {
   @Binding var data: Movie.FormData
+  @State var newPerformerName: String = ""
 
   var body: some View {
     Form {
@@ -12,6 +13,16 @@ struct MovieForm: View {
         }
       }
       .pickerStyle(.menu)
+      VStack(alignment: .leading) {
+        Text("Performers").modifier(FormLabel())
+        ForEach(data.performers) { Text($0.name) }
+        HStack {
+          TextField("New Performer", text: $newPerformerName)
+          Button { addPerformer(newPerformerName) } label: {
+            Image(systemName: "plus.circle.fill")
+          }
+        }
+      }
       TextFieldWithLabel(label: "Poster URL", text: $data.posterUrl, prompt: "Enter a URL")
       VStack(alignment: .leading) {
         Text("Synopsis")
@@ -20,6 +31,11 @@ struct MovieForm: View {
       }
       TextFieldWithLabel(label: "Directed By", text: $data.directedBy, prompt: "Who yelled 'Action!'")
     }
+  }
+
+  func addPerformer(_ name: String) {
+    data.performers.append(Performer(name: name))
+    newPerformerName = ""
   }
 }
 
